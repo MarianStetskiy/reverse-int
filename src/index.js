2818224bef9aba60454239f3c3cd550db1511da3
Marian <marian50391@gmail.com>
2022-07-16 15:13:57 +0300

    init: start task and added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let m = Math.abs(n);
+	let str1 = m.toString();
+	let str2 = str1.split('');
+	let str3 = str2.reverse();
+	let str4 = str3.join('');
+	return str4;
 }
